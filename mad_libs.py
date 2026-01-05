bf435e6d722782d0a4ad776963a7f94a5105c13e
Karabo Makganyoga <kamakjhb025@student.wethinkcode.co.za>
2026-01-05 15:00:09 +0200

    mad libs program using adjective

---
+++ mad_libs.py
@@ -0,0 +1,14 @@
+adjective1 = input("Enter your first adjective: ")
+adjective2 = input("Enter your second adjective: ")
+adjective3 = input("Enter your third adjective: ")
+adjective4 = input("Enter your last adjective: ")
+
+
+story = f"""On a beautiful {adjective1} day, I went to the zoo.
+I saw a funny {adjective2} monkey swinging from the trees.
+Then, I spotted a majestic {adjective3} lion lounging in the sun.
+What a wild and {adjective4} experience! """
+
+print(">>>>>> Zoo DAY <<<<<<<<")
+print("")
+print(story)
